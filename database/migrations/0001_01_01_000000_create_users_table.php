34ab24c13e24c4ebc7def0a615f72fb61c810358
ashkanrabiee <ashkanrabiee@gmail.com>
2025-06-29 03:35:43 -0700

    add Breeze to the project

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -14,6 +14,8 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('avatar')->default('/default/default.png');
+            $table->enum('user_type' , ['admin' , 'user'])->default('user');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
